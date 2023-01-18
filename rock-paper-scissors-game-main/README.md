ab0e9131dcc22ceac961a048da493ee08f58f97d
Jetch123 <jetchells1995@gmail.com>
2023-01-18 10:51:03 +0100

    First Commit

---
+++ rock-paper-scissors-game-main/README.md
@@ -0,0 +1,2 @@
+# rock-paper-scissors-game
+A simple fully responsive rock,paper and scissors game developed using HTML,CSS, JS for WSB Univeristy Poznan course.
---
+++ rock-paper-scissors-game-main/app.js
@@ -0,0 +1,170 @@
+// array for hand options
+const options = ["rock", "paper", "scissor"];
+let score = {
+  you: 0,
+  computer: 0
+};
+// colors of result messages
+const WIN_GREEN = "#6ac475";
+const LOSE_RED = "#c4736a";
+const DRAW_BLUE = "#5865f2";
+
+//function gets random integer
+function getRandomInt() {
+  return Math.floor(Math.random() * 3);
+}
+
+const btns = document.querySelectorAll(".options button");
+btns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const playerA = btn.querySelector("label").innerText;
+    const playerB = options[getRandomInt()];
+    compare(playerA, playerB);
+  });
+});
+
+// Function for comparing user/computer choice
+function compare(player, computer) {
+  const won = "YOU WON";
+  const lose = "YOU LOST";
+  const resultEl = document.querySelector(".hands .result");
+  const youScore = document.querySelector(".score .you");
+  const computerScore = document.querySelector(".score .computer");
+
+  //in the case of draw
+  if (player == computer) {
+    console.log(`${player} is equal to ${computer}`);
+    update(player, computer);
+    resultEl.style.color = DRAW_BLUE;
+    resultEl.innerText = "DRAW";
+
+    // player wins with rock > scissor
+  } else if (player == "rock" && computer == "scissor") {
+    console.log(`${player} defeats ${computer}`);
+    update(player, computer);
+    score.you++;
+    resultEl.style.color = WIN_GREEN;
+    resultEl.innerText = won;
+    youScore.innerText = score.you;
+
+    // player loses with rock < paper
+  } else if (player == "rock" && computer == "paper") {
+    console.log(`${player} loses to ${computer}`);
+    update(player, computer);
+    score.computer++;
+    resultEl.style.color = LOSE_RED;
+    resultEl.innerText = lose;
+    computerScore.innerText = score.computer;
+
+    // player loses with paper < scissor
+  } else if (player == "paper" && computer == "scissor") {
+    console.log(`${player} loses to ${computer}`);
+    update(player, computer);
+    score.computer++;
+    resultEl.style.color = LOSE_RED;
+    resultEl.innerText = lose;
+    computerScore.innerText = score.computer;
+
+    // player wins with paper > rock
+  } else if (player == "paper" && computer == "rock") {
+    console.log(`${player} defeats ${computer}`);
+    update(player, computer);
+    score.you++;
+    resultEl.style.color = WIN_GREEN;
+    resultEl.innerText = won;
+    youScore.innerText = score.you;
+
+    // player loses with scissor < rock
+  } else if (player == "scissor" && computer == "rock") {
+    console.log(`${player} loses to ${computer}`);
+    update(player, computer);
+    score.computer++;
+    resultEl.style.color = LOSE_RED;
+    resultEl.innerText = lose;
+    computerScore.innerText = score.computer;
+
+    // player wins with scissor > paper
+  } else if (player == "scissor" && computer == "paper") {
+    console.log(`${player} defeats ${computer}`);
+    update(player, computer);
+    score.you++;
+    resultEl.style.color = WIN_GREEN;
+    resultEl.innerText = won;
+    youScore.innerText = score.you;
+  }
+}
+function update(player, computer) {
+  const rock = "rock.png";
+  const paper = "paper.png";
+  const scissor = "scissor.png";
+
+  const playerHand = document.querySelector(".hands .player-hand");
+  const computerHand = document.querySelector(".hands .computer-hand");
+
+  // playerHand
+  if (player == "rock") {
+    playerHand.src = rock;
+  } else if (player == "paper") {
+    playerHand.src = paper;
+  } else if (player == "scissor") {
+    playerHand.src = scissor;
+  }
+
+  // computerHand
+  if (computer == "rock") {
+    computerHand.src = rock;
+  } else if (computer == "paper") {
+    computerHand.src = paper;
+  } else if (computer == "scissor") {
+    computerHand.src = scissor;
+  }
+}
+
+
+//Reset Game Button ---------------------------------
+const resetBtn = document.querySelector(".reset");
+resetBtn.addEventListener("click", () => {
+  score.you = 0;
+  score.computer = 0;
+  document.querySelector(".score .you").innerText = score.you;
+  document.querySelector(".score .computer").innerText = score.computer;
+  document.querySelector(".hands .result").innerText = "";
+  document.querySelector(".hands .player-hand").src = "rock.png";
+  document.querySelector(".hands .computer-hand").src = "rock.png";
+});
+
+
+// // Mapping
+// // 0 -> rock, 1->paper, 2->scissor
+// const options = ["rock", "paper", "scissor"];
+
+// // number
+// /**
+//  *
+//  * @param {number} player1Choice
+//  * @param {number} player2Choice
+//  * return string player1, player2, draw
+//  */
+// function whoWon(player1Choice, player2Choice) {
+//   /**
+//    * 0, 0 => draw
+//    * 0, 1 => rock
+//    * 0, 2 => scissor
+//    * 1, 0 => rock
+//    * 1, 1 => draw
+//    * 1, 2
+//    * 2, 0
+//    * 2, 1
+//    * 2, 2 => draw
+//    */
+// }
+
+// function generateComputerChoice() {
+//   let a = options[getRandomInt(3)];
+//   console.log(a);
+// }
+// generateComputerChoice();
+
+// function getRandomInt(max) {
+//   return Math.floor(Math.random() * max);
+// }
